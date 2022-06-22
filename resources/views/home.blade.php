6f19587bd805a4374aa5a94da9a00d69a1f3a081
ManuelBenini <manuelbenini1905@gmail.com>
2022-06-22 14:26:35 +0200

    Primo commit, esercizio terminato (senza bonus)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Homepage</title>
+</head>
+<body>
+    
+
+    <h1>Ciao {{$user}}! </h1>
+    <h2>Ecco cosa puoi fare oggi:</h2>
+
+    <ul>
+        @foreach ($azioni as $azione)
+
+            @if ($loop->first)
+                <li class="blue"> {{$azione}} </li>    
+            @elseif ($loop->last)
+                <li class="green"> {{$azione}} </li> 
+            @elseif ($azione == 'Urlare')
+                <li class="red"> {{$azione}} </li> 
+            @else
+                <li> {{$azione}} </li>
+            @endif
+
+
+        @endforeach
+        
+    </ul>
+
+
+</body>
+</html>
